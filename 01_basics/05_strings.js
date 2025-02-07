080c8d187a1e880b52ad23c625f2bc853513ce7b
Sparsh Rajvanshi <sparshraj077@gmail.com>
2025-02-07 06:25:56 +0000

    strings function

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,49 @@
+
+const name="hitesh--hc"
+const repoCount=50
+
+// console.log(name+ repoCount + "Value");
+// console.log(`hello my name is ${name} and my repo is${repoCount}`);
+
+
+const gameName= new String('sparshRajvanshi');
+
+// console.log(gameName[0]);
+// console.log(gameName.__pro__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+// substring
+// const newString=gameName.substring(0,4)
+//  console.log(newString);
+
+// // slice 
+// const anotherString = gameName.slice(-4, -1); 
+// console.log(anotherString); 
+
+// const newStringOne="         avengers assemble   "
+// console.log(newStringOne);
+
+// console.log(newStringOne.trim());
+
+// const url="https://hitesh.com/hitesh%20choudhary"
+// console.log(url.replace('%20','-'))
+// console.log(url.includes('sundar'));
+// console.log(gameName.split('-'));
+
+// slice 
+let arr=[1,2,3,4,5];
+console.log(arr.slice(1,4));
+console.log(arr);
+
+//  splice 
+let arr1=[1,2,3,4,5]
+console.log(arr.splice(1,4));
+console.log(arr);
+
+// split
+let sentence="hello buddy, how it's going";
+console.log(sentence.split(" "));
