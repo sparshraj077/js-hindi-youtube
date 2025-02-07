874e1e7875b4a20451a8bbb574a9703d1c799f35
Sparsh Rajvanshi <sparshraj077@gmail.com>
2025-02-07 04:59:21 +0000

    datttypes summary

---
+++ 01_basics/datatypes.summary.js
@@ -0,0 +1,48 @@
+/*
+there are two types of datatypes 
+// Primitive data type 
+7 types: String, Number, Boolean, null, undefined,Symbol, BigInt
+
+// Non primitive data type  or Reference Type 
+//Arrays , Objects, Functions
+ 
+*/
+const score=100
+const scoreValue=100.3
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail;
+const id =Symbol('123')
+//Symbol is a primitive data type introduced in ES6.
+//  It is used to create unique and immutable identifiers. 
+const anotherId=Symbol('123')
+console.log(id===anotherId)
+
+const bigNumber=65374378563856n
+console.log(bigNumber)
+
+// Reference (Non Primitive)
+
+// Array , Objects , Functions
+
+ const heros=["shaktiman","naagraj","doga"]
+  let myObj={
+    name:"sparsh",
+    age:22,
+ }
+
+ const myFunction=function(){
+    console.log("It's going great")
+ }
+ console.log(typeof bigNumber);
+
+ /*datatype basically typeof
+ undefined->undefined
+ Null ->object
+ booolean->boolean
+ number->number
+ String->String 
+ null->object
+//non -primitive datatype
+ // object , functions,array -> function 
+ */
