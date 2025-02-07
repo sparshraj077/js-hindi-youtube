88d1595ec434a59346729ff9de4b72ec84bd4cfc
Sparsh Rajvanshi <sparshraj077@gmail.com>
2025-02-07 04:29:01 +0000

    conversionoperationspart2

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,4 +23,37 @@ let booleanIsLoggedIn=Boolean(isLoggedIn)
 
 let number=33
 let stringNumber=String(number);
-console.log(typeof stringNumber);
+// 
+// console.log(typeof stringNumber);
+
+
+//********************************Operations**************** */
+
+let value=3
+let negValue =-value
+console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);power
+// console.log(2-2);
+// console.log(2%2); //remainder
+
+let str1="hello"
+let str2=" hitesh"
+let str3=str1+str2
+// console.log(str3);
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+1+2);
+// console.log(1+2+"2");
+
+console.log(true);
+// 
+let gameCounter = 100
+++gameCounter;
+gameCounter++;
+console.log(gameCounter);
