a72defdc9cb15221cbb6a07306d3e1814d9d8be4
Sparsh Rajvanshi <sparshraj077@gmail.com>
2025-02-07 04:12:06 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score="33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// conversion
+let valueInNumber=Number(score)
+// console.log( valueInNumber)
+
+//conversion results 
+
+// "33" =>33
+//"33abc" =>NaN
+//true =>1;false=>0;
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+
+// 1=>true;0=>false;
+// ""=>false
+//"hitesh"=>true
+
+let number=33
+let stringNumber=String(number);
+console.log(typeof stringNumber);
