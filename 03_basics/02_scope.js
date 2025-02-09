3401382cdcdaaf3018fafb21c2017491ac1e0993
Sparsh Rajvanshi <sparshraj077@gmail.com>
2025-02-09 14:15:47 +0000

    let , var  ,const -> local and global scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,19 @@
+let a= 10 
+const b=20
+var c =30
+
+// this is called as scope {}
+var c =300
+// global scope 
+if(true)
+{
+    // block scope
+    let a= 10 
+    const b=20
+    var c =30  
+}
+
+
+// console.log(a);
+// console.log(b);
+console.log(c);
